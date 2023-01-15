9927ae94436453f0b1184a297549635e2d094350
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-01-15 14:20:30 +0100

    Add main.py

--- 1 - Python/5 - Les fonctions/5.1 - Notions essentielles/5.1.2 - Exercice : Tables de multiplication/main.py
+++ 1 - Python/5 - Les fonctions/5.1 - Notions essentielles/5.1.2 - Exercice : Tables de multiplication/main.py
@@ -1,1 +1,27 @@
+# EXERCICE FONCTIONS
+# Tables de multiplication
+#
+#
+# 1 x 4 = 4
+# 2 x 4 = 8
+# 3 x 4 = 12
+# ...
+# 10 x 4 = 40
+#
+# afficher_table_multiplication(n)
+# min / max
+# erreur : si min > max => erreur
+
+
+def afficher_table_multiplication(n, min=1, max=10):
+    if min > max:
+        print("ERREUR : Le min est supéreur au max")
+        return
+
+    for i in range(min, max+1):
+        print(i, "x", n, "=", i*n)
+
+
+afficher_table_multiplication(5, 10, 1)
+
 
