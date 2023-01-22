d5b303f0b6fb4ff9df925a01f8be772a6814596b
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-01-22 13:38:23 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.2 - Demander des noms de personnes/main.py
+++ 1 - Python/6 - Les collections/6.2 - Demander des noms de personnes/main.py
@@ -1,1 +1,28 @@
+# LISTES - EXERCICE : DEMANDER NOMS DE PERSONNES
+
+
+# nom et l'age input
+
+'''for i in range(3):
+    nom = input("Nom de la personne : ")
+    print("Le nom est :", nom)'''
+
+# demander des noms de personnes
+# boucle infinie, sort quand le nom est vide == "" => break
+# seulement à la fin afficher tous les noms
+
+noms = []
+
+while True:
+    nom = input("Nom de la personne : ")
+    if nom == "":
+        break
+    noms.append(nom)
+
+print()
+print("Nom des personnes")
+noms.sort() # ordre alphabétique A-Z a-z
+for nom in noms:
+    print("  " + nom)
+
 
