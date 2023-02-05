655dec3a3694e098245fd22e19d45831a2cc7d62
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-05 13:48:18 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.1 - Append, extend, insert/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.1 - Append, extend, insert/main.py
@@ -1,1 +1,29 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Append / Extend / += / Insert
+
+# Append = ajouter un seul élément à la fin. Il ajouter telle quelle. Si on met une liste, comme ci-dessous, il ajoute cette liste sans réfléchir. Pour ajouter les deux éléments en tant que chaînes de caractères, de manière individuelle, boucler !
+# --> Pour faire cela plus rapdement qu'une boucle, utiliser "extend" au lieu de boucler.
+# --> += se comporte comme "extend" : il ajoute les éléments les uns après les autres
+# --> Insert[index] : insérer un élément au début ou au milieu d'une liste, au lieu de la fin, comme c'est le cas avec append
+
+noms = ["Jean", "Sophie", "Martin"]
+
+noms_supplementaires = ["Christophe", "Zoe"]
+
+#noms.append(noms_supplementaires)
+# for e in noms_supplementaires:
+#    noms.append(e)
+#noms.extend(noms_supplementaires)
+# noms += noms_supplementaires
+# noms = noms + noms_supplementaires  --> le +, c'est comme "extend". Attention, on concatène des éléments de même type, sinon ça ne marche pas ! Sur les listes, le + se comporte comme extend.
+
+# noms.append("Toto")
+# noms.insert(1, "Toto")
+
+noms = noms + noms_supplementaires
+
+print(noms)
+
+
+
 
