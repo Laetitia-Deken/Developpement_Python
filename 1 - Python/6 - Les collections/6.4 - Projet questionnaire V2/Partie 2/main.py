2cc0d5443eb4230d3bfd33ced43042f65f671c43
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-05 13:43:43 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.4 - Projet questionnaire V2/Partie 2/main.py
+++ 1 - Python/6 - Les collections/6.4 - Projet questionnaire V2/Partie 2/main.py
@@ -1,1 +1,64 @@
+# LES FONCTIONS : PROJET QUESTIONNAIRE
+#
+# Question : Quelle est la capitale de la France ?
+# (a) Marseille
+# (b) Nice
+# (c) Paris
+# (d) Nantes
+#
+# Votre réponse :
+# Bonne réponse / Mauvaise réponse
 
+# ...
+# Question : Quelle est la capitale de l'Italie ?
+# ...
+#
+# 4 questions
+
+'''
+titre = question[0]
+choix = question[1]
+bonne_reponse = question[2]
+'''
+def poser_question(question):
+    # titre_question, r1, r2, r3, r4, choix_bonne_reponse
+    choix = question[1]
+    bonne_reponse = question[2]
+    global score
+    print("QUESTION")
+    print("  " + question[0])
+    print("  ", choix[0])
+    print("  ", choix[1])
+    print("  ", choix[2])
+    print("  ", choix[3])
+    print()
+    reponse = input("Votre réponse : ")
+    if reponse.lower() == bonne_reponse.lower():
+        print("Bonne réponse")
+        score += 1
+    else:
+        print("Mauvaise réponse")
+        
+    print()
+
+
+score = 0
+
+'''
+    questionnaire[]
+        question
+            titre = "Quelle est la capitale de la France ?"
+            reponses = ("Marseille", "Nice", "Paris", "Nantes")
+            bonne_reponse = "Paris"
+
+'''
+
+question1 = ("Quelle est la capitale de la France ?", ("Marseille", "Nice", "Paris", "Nantes"), "Paris")
+question2 = ("Quelle est la capitale de la l'Italie ?", ("Rome", "Venise", "Pise", "Florence"), "Rome")
+
+poser_question(question1)
+poser_question(question2)
+# poser_question("Quelle est la capitale de la France ?", "Marseille", "Nice", "Paris", "Nantes", "c")
+#poser_question("Quelle est la capitale de l'Italie ?", "Rome", "Venise", "Pise", "Florence", "a")
+
+print("Score final :", score)
