3960d33d4a53395b4fb050ee5d21ca112e10e73c
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-12 13:33:36 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.5 - Swapper deux éléments d'une liste/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.5 - Swapper deux éléments d'une liste/main.py
@@ -1,1 +1,15 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Swapper deux éléments (échanger deux éléments dans une liste)
+# Attention au rique d'écrasement ! Conseillé de créer une variable intermédiare pour sauvegarder la liste originale avant de swapper
 
+#          0        1        2           3           4
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe"]
+
+'''t = noms[0]  # Jean
+noms[0] = noms[4]  # Jean <- Zoe
+noms[4] = t'''
+
+#                     Zoe      Jean
+noms[0], noms[4] = noms[4], noms[0] # récupère les valeurs en une fois, on n'écrase plus une valeur qu'on va perdre
+
+print(noms)
