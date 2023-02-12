8a6f4b6e7d814a6a20a93b7ddc2ca7feedddbf00
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-12 13:34:32 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.6 - Join et Split/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.6 - Join et Split/main.py
@@ -1,1 +1,20 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# join et split
 
+#          0        1        2           3           4
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe"]
+
+# Join : Rejoindre -> coller les éléments de la collection. Prend les différents éléments d'une collection et vient les coller ensemble, grâce à un caractère comme le "-" par exemple. Cette fonction concatène chaque chaîne de caractères, avec le "-", par exemple.
+
+noms_join = ", ".join(noms)
+print(noms_join)
+
+# Split : Séparer, en liste avec les éléments séparés.
+# a = "Paul-Marc-Emilie"
+#a_split = a.split("-")
+#print(a_split)
+
+noms_resconstruits = noms_join.split(", ")
+print(noms_resconstruits)
+
+# Split est l'inverse du join.
