25cd5b3c90f3fa11533790b108898382615a6077
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-12 13:29:38 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.2 - Précisions sur les slices/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.2 - Précisions sur les slices/main.py
@@ -1,1 +1,18 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Slices = sélectionnent certains éléments dans une liste.
+
+#          0        1        2           3           4
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe"]
+
+slices_noms = noms[-2:] # [inclus:exclu]
+slices_noms = noms[1:] # démarre à 1 jusqu'à la fin de la liste. Et on fait une copie de la liste noms avec les mêmes valeurs en mémoire. On a donc 2 listes = la liste noms et la liste recopiée sclices_noms.
+
+#noms[0] = "Toto"
+
+print(slices_noms)
+#print(noms)
+
+# QUAND ON FAIT UN SLICE, ON CREE UNE NOUVELLE LISTE !!
+
+
 
