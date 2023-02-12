d4988833e10c044b1707865aa819aaa2076769c4
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-12 13:30:34 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.3 - Tris sort et sorted/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.3 - Tris sort et sorted/main.py
@@ -1,1 +1,21 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Tris : Sort / Sorted
+# sort() --> tri dans l'ordre alphabétique, directement dans une liste, ici noms. On n'a pas copié la liste. C'est une opération "in place".
+# sorted --> prend une collection, ici noms et créé une nouvelle liste, qui va être triée. La liste originale est préservée.
+
+def tri_longeur_caracteres(nom):
+    return len(nom)
+
+#          0        1        2           3           4
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe"]
+
+# tri par ordre alphabetique STR
+# du plus petit au plus grand INT
+#noms.sort(key=tri_longeur_caracteres, reverse=True)  # inplace
+noms_tries = sorted(noms, key=tri_longeur_caracteres, reverse=True)  # créer une nouvelle liste
+# reverse = True --> ordre dans l'ordre alphabétique inversé
+
+print(noms)
+print(noms_tries)
+
 
