f7fca9f836e940f056c3e7466ff88cccb02f9796
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-19 13:30:43 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.10 - In insensible à la casse/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.10 - In insensible à la casse/main.py
@@ -1,1 +1,20 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Exercice "in insensible à la casse"
 
+
+def element_dans_liste(e, l):
+    '''for el in l:
+        if e.lower() == el.lower():
+            return True
+    return False'''
+    l_lower = [el.lower() for el in l]
+    #print(l_lower)
+    return e.lower() in l_lower
+
+#          0        1        2           3           4      5
+noms = ["Jean", "Sophie", "Martin", "Christophe", "Zoe", "Martin"]
+
+if element_dans_liste("jeANffd", noms):
+    print("Element trouvé")
+else:
+    print("Element non trouvé")
