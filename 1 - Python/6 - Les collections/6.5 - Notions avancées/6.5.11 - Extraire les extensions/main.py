e51b36216a8c448216949d38dfb261a1ea92c461
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-19 13:31:41 +0100

    Add main.py

--- 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.11 - Extraire les extensions/main.py
+++ 1 - Python/6 - Les collections/6.5 - Notions avancées/6.5.11 - Extraire les extensions/main.py
@@ -1,1 +1,58 @@
+# LES COLLECTIONS : LISTES / TUPLES
+# Exercice "Extraire les extensions"
+
+def extraire_extension(nom_fichier):
+    nom_fichier_split = nom_fichier.split(".")
+    if len(nom_fichier_split) > 1:
+        return nom_fichier_split[-1]
+    return None
+
+def obtenir_definition_extension(extension, definitions):
+    for d in definitions:
+        if d[0].lower() == extension.lower():
+            return d[1]
+    return None
+
+fichiers = ("notepad.exe", "mon.fichier.perso.doc", "notes.TXT", "vacances.jpeg", "planning", "data.dat")
+
+definition_extensions = (("doc", "Document Word"),
+                        ("exe", "Executable"),
+                        ("txt", "Document Texte"),
+                        ("jpeg", "Image JPEG"))
+
+"""definition_extensions_dict = {"doc": "Document Word",
+                        "exe": "Executable",
+                        "txt": "Document Texte",
+                        "jpeg": "Image JPEG"}"""
+
+for fichier in fichiers:
+    ext = extraire_extension(fichier)
+    if ext:
+        definition = obtenir_definition_extension(ext, definition_extensions)
+        # definition = definition_extensions_dict.get(ext.lower())
+        if not definition:
+            definition = "Extension non connue"
+    else:
+        definition = "Aucune extension"
+    print(fichier + " (" + definition + ")")
+
+'''
+notepad.exe (Executable)
+mon.fichier.perso.doc (Document Word)
+notes.TXT (Document Texte)
+vacances.jpeg (Image JPEG)
+planning (Aucune extension)
+data.dat (Extension non connue)
+'''
+
+# lower/upper 
+# in / index / for
+# split
+# -1
+
+# extraire extension
+
+
+
+# faire la correspondance définition
 
