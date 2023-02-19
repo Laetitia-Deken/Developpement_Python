4b5e86fd08a94f0df5b3836bec1e4dc357315133
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-02-19 13:35:06 +0100

    Add main.py

--- 1 - Python/7 - Programmation Orientée Objet (POO)/7.1 - Premiers pas en POO/main.py
+++ 1 - Python/7 - Programmation Orientée Objet (POO)/7.1 - Premiers pas en POO/main.py
@@ -1,1 +1,33 @@
+###
+# VOS PREMIERS PAS EN POO (PROGRAMMATION ORIENTÉE OBJET)
+###
+# - Différence programmation impérative (demander...) --> à transformer en objet
+# - Le plus simple possible
+# - Exercices, mises en situation
 
+# Personne  (entité -> class) entité -> objet. Il faut raisonner sur les entités en POO. On fait en sorte que les fonctions puissent accéder à certaines données par rapport à l'entité en question.
+#    Données : nom, age
+#    Actions :  (méthodes)
+#       - SePresenter()
+#       - DemanderNom() / input
+#  [Personne "Jean"]     [Personne "Paul"]
+
+def afficher_informations_personne(nom, age): # Découpage entre les fonctions qui exécutent du code et les données
+    print(f"La personne s'appelle {nom}, son age est {age} ans")
+
+def demander_nom_personne():
+    nom = input("Quel est votre nom ?")
+    return nom
+
+nom1 = "Jean"
+age1 = 30
+
+nom2 = "Paul"
+age2 = 25
+
+afficher_informations_personne(nom1, age1)
+afficher_informations_personne(nom2, age2)
+
+nom3 = demanger_nom_personne()
+age3 = 18
+afficher_informations_personne(nom3, age3)
