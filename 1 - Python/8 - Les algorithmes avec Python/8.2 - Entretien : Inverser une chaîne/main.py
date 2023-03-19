5398b926c51900e3c083e8422f12c099b763b784
Laetitia DEKEN <88583822+Laetitia-Deken@users.noreply.github.com>
2023-03-19 13:40:09 +0100

    Add files

--- 1 - Python/8 - Les algorithmes avec Python/8.2 - Entretien : Inverser une chaîne/main.py
+++ 1 - Python/8 - Les algorithmes avec Python/8.2 - Entretien : Inverser une chaîne/main.py
@@ -1,1 +1,26 @@
+# Inverser une chaine (Reverse)
+#
+# "Bonjour toto"
+# "otot ruojnoB"
+#
+# Boucle
+# Slice
 
+# Boucle
+
+def reverse_string1(str):
+    r = ""
+    for c in str:
+        r = c + r
+    return r
+
+# Slice
+
+def reverse_string2(str):
+    return s[::-1] # + rapide à exécuter car - d'instructions
+
+
+s = "Bonjour toto"
+# print(reverse_string1(s))
+
+print(reverse_string2(s))
